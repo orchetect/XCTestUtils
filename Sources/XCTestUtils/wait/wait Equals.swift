2b1f9b9c6863c43eaf5e46ffc3d4194e9a9813b4
Steffan Andrews <stef@orchetect.com>
2024-11-25 04:31:38 -0800

    Added async variants of `wait` methods

--- Sources/XCTestUtils/wait/wait Equals.swift
+++ Sources/XCTestUtils/wait/wait Equals.swift
@@ -59,6 +59,55 @@ extension XCTestCase {
     /// Wait for an equality condition to be true, with a timeout period.
     ///
     /// Polling defaults to every 10 milliseconds, but can be overridden.
+    public func wait<T>(
+        for lhs: @autoclosure () async throws -> T,
+        equals rhs: @autoclosure () async throws -> T,
+        timeout: TimeInterval,
+        polling: TimeInterval = 0.010,
+        _ message: @autoclosure () -> String = "",
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) async rethrows where T: Equatable {
+        let inTime = Date()
+        let timeoutTime = inTime + timeout
+        
+        var continueLooping = true
+        var timedOut = false
+        
+        while continueLooping {
+            if Date() >= timeoutTime {
+                continueLooping = false
+                timedOut = true
+                continue
+            }
+            
+            let conditionResult = try await lhs() == rhs()
+            continueLooping = !conditionResult
+            if !continueLooping { continue }
+            
+            wait(sec: polling)
+        }
+        
+        if timedOut {
+            var msg = message()
+            msg = msg.isEmpty ? "" : ": \(msg)"
+            
+            let evaluatedLHS = try await lhs()
+            let evaluatedRHS = try await rhs()
+            XCTAssertEqual(
+                evaluatedLHS,
+                evaluatedRHS,
+                "wait timeout",
+                file: file,
+                line: line
+            )
+            return
+        }
+    }
+    
+    /// Wait for an equality condition to be true, with a timeout period.
+    ///
+    /// Polling defaults to every 10 milliseconds, but can be overridden.
     @_disfavoredOverload
     public func wait<T>(
         for lhs: () throws -> T,
@@ -96,6 +145,56 @@ extension XCTestCase {
             try XCTAssertEqual(
                 lhs(),
                 rhs(),
+                "wait timeout",
+                file: file,
+                line: line
+            )
+            return
+        }
+    }
+    
+    /// Wait for an equality condition to be true, with a timeout period.
+    ///
+    /// Polling defaults to every 10 milliseconds, but can be overridden.
+    @_disfavoredOverload
+    public func wait<T>(
+        for lhs: () async throws -> T,
+        equals rhs: () async throws -> T,
+        timeout: TimeInterval,
+        polling: TimeInterval = 0.010,
+        _ message: @autoclosure () -> String = "",
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) async rethrows where T: Equatable {
+        let inTime = Date()
+        let timeoutTime = inTime + timeout
+        
+        var continueLooping = true
+        var timedOut = false
+        
+        while continueLooping {
+            if Date() >= timeoutTime {
+                continueLooping = false
+                timedOut = true
+                continue
+            }
+            
+            let conditionResult = try await lhs() == rhs()
+            continueLooping = !conditionResult
+            if !continueLooping { continue }
+            
+            wait(sec: polling)
+        }
+        
+        if timedOut {
+            var msg = message()
+            msg = msg.isEmpty ? "" : ": \(msg)"
+            
+            let evaluatedLHS = try await lhs()
+            let evaluatedRHS = try await rhs()
+            XCTAssertEqual(
+                evaluatedLHS,
+                evaluatedRHS,
                 "wait timeout",
                 file: file,
                 line: line
